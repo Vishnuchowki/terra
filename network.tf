b54438a302f27471172a43714fa4d335b575136b
Vishnuchowki <mailatvishnuchowki@gmail.com>
2023-03-31 00:06:08 +0530

    adding terraform.workspace

--- network.tf
+++ network.tf
@@ -11,6 +11,6 @@ resource "aws_vpc" "ntier" {
 resource "aws_subnet" "basion" {
   count = terraform.workspace == "qa" ? 1 :0
   vpc_id = aws_vpc.ntier
-  cidr_block = "10.10.255.0"
+  cidr_block = "10.10.255.0/24"
   
 }
